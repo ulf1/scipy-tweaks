6047e9a673b0a93b870f20a0c173620c6f022b6f
UH <554c46@gmail.com>
2021-04-08 11:19:54 +0200

    rst for pypi, zip file install

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
-
-
-def read(fname):
-    import os
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import pypandoc
 
 
 setup(name='scipy-tweaks',
       version='0.1.1',
       description='Utility functions for scipy.',
-      long_description=read('README.md'),
-      long_description_content_type='text/markdown',
+      long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/scipy-tweaks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -20,4 +15,4 @@ setup(name='scipy-tweaks',
           'setuptools>=40.0.0',
           'scipy>=1.5.4'],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
