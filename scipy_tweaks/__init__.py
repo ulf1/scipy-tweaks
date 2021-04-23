04a4a47e70dfc6fad116fb426fd4cf1a401f9e2a
UH <554c46@gmail.com>
2021-04-24 01:03:08 +0200

    remove pandoc from setup.py

--- scipy_tweaks/__init__.py
+++ scipy_tweaks/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .idseqs_to_mask import idseqs_to_mask
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-import pypandoc
 
 
 def get_version(path):
@@ -15,7 +14,7 @@ def get_version(path):
 setup(name='scipy-tweaks',
       version=get_version("scipy_tweaks/__init__.py"),
       description='Utility functions for scipy.',
-      long_description=pypandoc.convert('README.md', 'rst'),
+      long_description='README.rst',
       url='http://github.com/ulf1/scipy-tweaks',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
@@ -24,4 +23,4 @@ setup(name='scipy-tweaks',
       install_requires=[
           'scipy>=1.5.4,<2'],
       python_requires='>=3.6',
-      zip_safe=False)
+      zip_safe=True)
